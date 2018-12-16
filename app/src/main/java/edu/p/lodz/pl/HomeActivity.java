99569fa0289efb2b7c618beb5091ae8a429cf6f6
mladra <michal.ladra.95@gmail.com>
2018-12-16 22:06:52 +0100

    Migratet to Android X. Created home view.

--- app/src/main/java/edu/p/lodz/pl/HomeActivity.java
+++ app/src/main/java/edu/p/lodz/pl/HomeActivity.java
@@ -1,6 +1,6 @@
 package edu.p.lodz.pl;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
