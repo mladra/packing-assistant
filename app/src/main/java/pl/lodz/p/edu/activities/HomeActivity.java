958b3145d7da5643b66c4e6807a69b8338b4d15d
mladra <michal.ladra.95@gmail.com>
2019-02-16 17:40:38 +0100

    Removed to do.

--- app/src/main/java/pl/lodz/p/edu/activities/HomeActivity.java
+++ app/src/main/java/pl/lodz/p/edu/activities/HomeActivity.java
@@ -20,23 +20,4 @@ public class HomeActivity extends AbstractActivity<ActivityHomeBinding> {
         this.binding.setPackingListsHandler(new ViewTransitionClickHandler(this, PackingListsActivity.class));
         this.binding.setTemplatesHandler(new ViewTransitionClickHandler(this, TemplatesActivity.class));
     }
-
-    //TODO: mladra: [X] LISTS -> Change single list view and add click handler in order to open edit list activity - 20min
-    //TODO: mladra: [X] ITEMS -> Change single item layout (show name, weight, activity) - 15min
-    //TODO: mladra: [] ITEMS -> Add weight into creation dialog - 1h
-    //TODO: mladra: [] ADD PACKING LIST -> Add current weight - 1h
-    //TODO: mladra: [X] ITEMS -> Change screen orientation to portrait only - 2min
-    //TODO: mladra: [REJECTED] ITEMS -> Add possibility to remove item - 1h (za dużo zależności, aby usunąć przedmiot)
-    //TODO: mladra: [] ITEMS -> Add possibility to edit item - 4h
-    //TODO: mladra: [] ITEMS -> Add validation on item addition (weather, activity) - 1.5h
-    //TODO: mladra: [] TEMPLATES -> Add empty list message - 0.5h
-    //TODO: mladra: [] TEMPLATES -> Add backend logic for template addition - 2h
-    //TODO: mladra: [] TEMPLATES -> Add possibility to remove template - 1h
-    //TODO: mladra: [] ADD LIST -> Integrate with Yahoo Weather and Place API - 8h
-    //TODO: mladra: [X] ADD LIST -> Add possibility to close list - 20min
-    //TODO: mladra: [X] ADD LIST -> Add possibility to save list - 30min
-    //TODO: mladra: [] ADD LIST -> Add possibility to create from template - 1h
-    //TODO: mladra: [X] LISTS -> Change status color depending on status - 5min
-    //TODO: mladra: [X] ADD_PACKING_LIST -> After choosing activities it was possible to back to choosing activities and create many packing lists
-    //TODO: mladra: [] LISTS -> Change single packing lists layout (created, destination)
 }
