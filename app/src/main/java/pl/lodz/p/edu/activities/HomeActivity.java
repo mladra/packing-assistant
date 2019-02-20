250efa8bf9d0f411cdec9c0b7a84b9ede2abd86f
mladra <michal.ladra.95@gmail.com>
2019-02-20 21:02:48 +0100

    Removed todos.

--- app/src/main/java/pl/lodz/p/edu/activities/HomeActivity.java
+++ app/src/main/java/pl/lodz/p/edu/activities/HomeActivity.java
@@ -18,27 +18,4 @@ public class HomeActivity extends AbstractActivity<ActivityHomeBinding> {
         this.binding.setPackingListsHandler(new ViewTransitionClickHandler(this, PackingListsActivity.class));
         this.binding.setTemplatesHandler(new ViewTransitionClickHandler(this, TemplatesActivity.class));
     }
-
-    //TODO: mladra: [] LIST -> Change created packing list view - 2h
-    //TODO: mladra: [] ITEMS -> Change single item view - 2h
-    //TODO: mladra: [] ITEMS -> Add possibility to edit item - 4h
-    //TODO: mladra: [] TEMPLATES -> Add empty list message - 0.5h
-    //TODO: mladra: [X] LIST -> Add required items condition before closing list - IT COULD BE HARD
-    //TODO: mladra: [X] ADD LIST -> Integrate with Yahoo Weather and Place API - 8h - IT COULD HARDER
-    //TODO: mladra: [X] ITEMS -> Add validation on item addition (weather, activity) - 10min
-    //TODO: mladra: [X] LIST -> Change date format for more human - 10min
-    //TODO: mladra: [X] ADD PACKING LIST -> Add current weight - 45min
-    //TODO: mladra: [X] ITEMS -> Add weight into creation dialog - 15min
-    //TODO: mladra: [X] ITEMS -> Change single item layout (show name, weight, activity) - 15min
-    //TODO: mladra: [X] ITEMS -> Change screen orientation to portrait only - 2min
-    //TODO: mladra: [X] LISTS -> Change single list view and add click handler in order to open edit list activity - 20min
-    //TODO: mladra: [X] TEMPLATES -> Add backend logic for template addition - 1h
-    //TODO: mladra: [X] ADD LIST -> Add possibility to close list - 20min
-    //TODO: mladra: [X] ADD LIST -> Add possibility to save list - 30min
-    //TODO: mladra: [X] ADD LIST -> Add possibility to create from template - 1h
-    //TODO: mladra: [X] LISTS -> Change status color depending on status - 5min
-    //TODO: mladra: [X] ADD_PACKING_LIST -> After choosing activities it was possible to back to choosing activities and create many packing lists - 15min
-    //TODO: mladra: [X] LISTS -> Change single packing lists layout (created, destination) - 20min
-    //TODO: mladra: [REJECTED] TEMPLATES -> Add possibility to remove template (za dużo zależności, stracilibyśmy istniejące listy)
-    //TODO: mladra: [REJECTED] ITEMS -> Add possibility to remove item (za dużo zależności, aby usunąć przedmiot, stracilibyśmy informacje w istniejących listach)
 }
